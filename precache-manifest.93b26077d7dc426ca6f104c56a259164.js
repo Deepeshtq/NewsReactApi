88073a1406379cd00e60188e5605db6540a4e949
Deepeshtq <tq9439434185@gmail.com>
2023-02-17 22:28:09 +0530

    Updates

---
+++ precache-manifest.93b26077d7dc426ca6f104c56a259164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f919f013879215e237f4",
+    "url": "/NewsReactApi/static/css/main.a2b8ccf5.chunk.css"
+  },
+  {
+    "revision": "f919f013879215e237f4",
+    "url": "/NewsReactApi/static/js/main.f919f013.chunk.js"
+  },
+  {
+    "revision": "b965d220e0d6421fd4e0",
+    "url": "/NewsReactApi/static/js/1.b965d220.chunk.js"
+  },
+  {
+    "revision": "9bb22382a70deb6d7df8",
+    "url": "/NewsReactApi/static/js/2.9bb22382.chunk.js"
+  },
+  {
+    "revision": "058f4d1174216a19eccc",
+    "url": "/NewsReactApi/static/js/runtime~main.058f4d11.js"
+  },
+  {
+    "revision": "58fb5f753a3f7ea26eed5a44c1b3c56e",
+    "url": "/NewsReactApi/static/media/loading.58fb5f75.gif"
+  },
+  {
+    "revision": "b65cbd7ec19aeb60f329d2a1904f5f31",
+    "url": "/NewsReactApi/index.html"
+  }
+];
